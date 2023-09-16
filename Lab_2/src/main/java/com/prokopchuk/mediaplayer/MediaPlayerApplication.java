2818edac1c63c61e72724f62376b2c88584ca4e7
Roman Prokopchuk <ghost62727@gmail.com>
2023-09-16 14:39:55 +0300

    Lab-2: Define UI components

--- Lab_2/src/main/java/com/prokopchuk/mediaplayer/MediaPlayerApplication.java
+++ Lab_2/src/main/java/com/prokopchuk/mediaplayer/MediaPlayerApplication.java
@@ -10,11 +10,13 @@ import java.io.IOException;
 public class MediaPlayerApplication extends Application {
 
   private static final String STAGE_TITLE = "Media player";
+  private static final int SCENE_WIDTH = 700;
+  private static final int SCENE_HEIGHT = 450;
 
   @Override
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(MediaPlayerApplication.class.getResource("hello-view.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    Scene scene = new Scene(fxmlLoader.load(), SCENE_WIDTH, SCENE_HEIGHT);
     stage.setTitle(STAGE_TITLE);
     stage.setScene(scene);
     stage.show();
