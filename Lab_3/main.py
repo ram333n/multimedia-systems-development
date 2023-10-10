9da37c50fad2bbb0da81eb10c2a8a45af0aadff3
Roman Prokopchuk <ghost62727@gmail.com>
2023-10-10 23:34:42 +0300

    Lab-3: Add wavelet transform

--- Lab_3/main.py
+++ Lab_3/main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 def read_image(image_name):
     return cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
 
@@ -59,6 +60,54 @@ class DctCompressor:
         return 1 / np.sqrt(2) if index == 0 else 1
 
 
+class WaveletCompressor:
+    def __init__(self, block_size=BLOCK_SIZE):
+        self.block_size = block_size
+
+    def compress(self, input_image_name, output_image_name):
+        input_image = read_image(input_image_name)
+        result_image = self.__compress(input_image)
+        write_image(output_image_name, result_image)
+
+    def __compress(self, input_image):
+        height, width = input_image.shape
+        result_image = np.ones_like(input_image)
+
+        for i in range(0, height, self.block_size):
+            for j in range(0, width, self.block_size):
+                current_block = input_image[i:i + self.block_size, j:j + self.block_size]
+                dct_block = self.__wavelet(current_block)
+                result_image[i:i + self.block_size, j:j + self.block_size] = dct_block
+
+        return result_image
+
+    def __wavelet(self, block):
+        length = len(block)
+        h = length // 2
+
+        LL = block[:h, :h]
+        LH = block[:h, h:]
+        HL = block[h:, :h]
+        HH = block[h:, h:]
+
+        LL_dwt = np.mean(LL)
+        LH_dwt = LH - np.mean(LH)
+        HL_dwt = HL - np.mean(HL)
+        HH_dwt = HH - np.mean(HH)
+
+        common_shape = LH.shape
+
+        LL_dwt = np.full(common_shape, LL_dwt)
+        LH_dwt = LH_dwt
+
+        result = np.vstack((
+            np.hstack((LL_dwt, LH_dwt)),
+            np.hstack((HL_dwt, HH_dwt))
+        ))
+
+        return result
+
+
 def psnr(first_image, second_image, max_pixel=255):
     mse = np.mean((first_image - second_image) ** 2)
 
@@ -72,9 +121,10 @@ def psnr_by_names(first_image_name, second_image_name, max_pixel=255):
     return psnr(first_image, second_image, max_pixel)
 
 
-def demo(input_image_name, dct_image_name, dwt_image_name):
+def demo(input_image_name, dct_image_name, wavelet_image_name):
     input_image = read_image(input_image_name)
     dct_image = read_image(dct_image_name)
+    wavelet_image = read_image(wavelet_image_name)
 
     fig, axes = plt.subplots(1, 3, figsize=(12, 6))
 
@@ -86,20 +136,30 @@ def demo(input_image_name, dct_image_name, dwt_image_name):
     axes[1].set_title(f'DCT algorithm(PSNR={psnr(input_image, dct_image):.3f} dB)')
     axes[1].axis('off')
 
+    axes[2].imshow(wavelet_image, cmap='gray')
+    axes[2].set_title(f'Wavelet algorithm(PSNR={psnr(input_image, wavelet_image):.3f} dB)')
+    axes[2].axis('off')
+
     plt.show()
 
 
 def main():
     input_image_name = 'input_image.png'
-    output_image_name_dct = 'dct_compressed_2.png'
+    output_image_name_dct = 'dct_compressed.png'
     dct_compressor = DctCompressor()
+
+    output_image_name_wavelet = "wavelet_compressed.png"
+    wavelet_compressor = WaveletCompressor()
 
     print('Started compressing by DCT')
     dct_compressor.compress(input_image_name, output_image_name_dct)
     print('Finished compressing by DCT')
-    print(f'PSNR: {psnr_by_names(input_image_name, output_image_name_dct)} dB')
 
-    demo(input_image_name, output_image_name_dct, None)
+    print('Started compressing by wavelet')
+    wavelet_compressor.compress(input_image_name, output_image_name_wavelet)
+    print('Finished compressing by wavelet')
+
+    demo(input_image_name, output_image_name_dct, output_image_name_wavelet)
 
 
 if __name__ == '__main__':
